f17ec9465efec4f02c1a5965d0b8a315bd0c58a6
Diogo V. Kersting <diogovk@gmail.com>
2016-04-17 10:07:38 -0300

    Configure port when running execute_script.sh

--- execute_script.sh
+++ execute_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-scp -i ~/.ssh/udacity_key.rsa ./setup_server.bash root@52.26.115.20:
-ssh -i ~/.ssh/udacity_key.rsa root@52.26.115.20 bash '~/setup_server.bash'
+SSH_PORT=2200
+
+set -e
+scp -P $SSH_PORT -i ~/.ssh/udacity_key.rsa ./setup_server.bash root@52.26.115.20:
+ssh -p $SSH_PORT -i ~/.ssh/udacity_key.rsa root@52.26.115.20 bash '~/setup_server.bash'
 
